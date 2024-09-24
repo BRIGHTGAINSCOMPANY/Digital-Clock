3956c787b7a9dd1ced128e576685e25b3ef289ab
BRIGHTGAINSCOMPANY <162323582+BRIGHTGAINSCOMPANY@users.noreply.github.com>
2024-09-24 08:19:50 +0300

    watch project

---
+++ clock.py
@@ -0,0 +1,34 @@
+import sys
+from customtkinter import *
+import time
+
+def times():
+    current_time = time.strftime("%a- %I:%M:%S %p")
+    
+    clock.configure(text = current_time)
+   
+    clock.after(200, times)
+
+root = CTk()
+root.geometry("630x230")
+root.title("Digital Clock")
+
+
+
+font1= ('Arial', 53, 'bold')
+font2 =('Arial', 29, 'bold')
+font3 = ('Times', 30, 'bold')
+
+clock = CTkLabel(root, font = font1)
+clock.grid(row=2, column=2, pady=25, padx=100)
+
+times()
+
+title = CTkLabel(root, text="TIME IS:", font = font2)
+title.grid(row = 1, column = 2, pady=10, padx= 100)
+
+bottom_text = CTkLabel(root, text = "H     M     S", font= font3)
+bottom_text.grid(row=3, column=2)
+
+
+root.mainloop()
